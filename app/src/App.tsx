07002d75466e0c11c94506bfcf544dd887538304
aereeeee <dofl5576@gmail.com>
2023-11-19 14:57:03 -0500

    Init webapp

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import "normalize.css";
+
+function App() {
+  return (
+    <div>
+      <div className="section">
+        <div className="title-wrapper"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
