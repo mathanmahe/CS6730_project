601a6ffa11bdba116cc5ea67e334e1b4ce1c5bd6
shreya6064 <77981993+shreya6064@users.noreply.github.com>
2023-12-03 22:33:49 -0500

    Add files via upload

--- posters/annotate.py
+++ posters/annotate.py
@@ -1,12 +1,226 @@
 import cv2, os
+import numpy as np
 
-for i in sorted(os.listdir('.')):
 
-    if i[-3:] == 'jpg':
+class Mouse_Click_Correspondence(object):
+
+    def __init__(self,path1='',path2='',img1='',img2=''):
+        self.sx1 = []
+        self.sy1 = []
+        self.sx2 = []
+        self.sy2 = []
+        self.img = img1
+        self.img2 = img2
+        self.path1 = path1
+        self.path2 = path2
+
+
+    def click_event(self,event, x, y, flags, params):
+        # checking for left mouse clicks
+        if event == cv2.EVENT_LBUTTONDOWN:
+            # displaying the coordinates
+            # on the Shell
+            print('x y', x, ' ', y)
+
+            sx1=self.sx1
+            sy1=self.sy1
+
+            sx1.append(x)
+            sy1.append(y)
+
+            # displaying the coordinates
+            # on the image window
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(self.img, str(x) + ',' +
+                        str(y), (x, y), font,
+                        1, (255, 0, 0), 2)
+            cv2.imshow('image 1', self.img)
+
+            # checking for right mouse clicks
+        if event == cv2.EVENT_RBUTTONDOWN:
+            # displaying the coordinates
+            # on the Shell
+            print(x, ' ', y)
+
+            # displaying the coordinates
+            # on the image window
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            b = self.img[y, x, 0]
+            g = self.img[y, x, 1]
+            r = self.img[y, x, 2]
+            cv2.putText(self.img, str(b) + ',' +
+                        str(g) + ',' + str(r),
+                        (x2, y2), font, 1,
+                        (255, 255, 0), 2)
+            cv2.imshow('image 1', self.img)
+
+        # driver function
+
+    def click_event2(self,event2, x2, y2, flags, params):
+        # checking for left mouse clicks
+        if event2 == cv2.EVENT_LBUTTONDOWN:
+            # displaying the coordinates
+            # on the Shell
+            print('x2 y2', x2, ' ', y2)
+
+            sx2= self.sx2
+            sy2 = self.sy2
+
+            sx2.append(x2)
+            sy2.append(y2)
+
+            # displaying the coordinates
+            # on the image window
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(self.img2, str(x2) + ',' +
+                        str(y2), (x2, y2), font,
+                        1, (0, 255, 255), 2)
+            cv2.imshow('image 2', self.img2)
+
+            # checking for right mouse clicks
+        if event2 == cv2.EVENT_RBUTTONDOWN:
+            # displaying the coordinates
+            # on the Shell
+            print(x2, ' ', y2)
+
+            # displaying the coordinates
+            # on the image window
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            b = self.img2[y2, x2, 0]
+            g = self.img2[y2, x2, 1]
+            r = self.img2[y2, x2, 2]
+            cv2.putText(self.img2, str(b) + ',' +
+                        str(g) + ',' + str(r),
+                        (x2, y2), font, 1,
+                        (255, 255, 0), 2)
+            cv2.imshow('image 2', self.img2)
+
+    # driver function
+    def driver(self,path1,path2):
+        # reading the image
+        # path = r'D:\GaTech\TA - CV\ps05\ps05\ps5-1-b-1.png'
+        #path1 = r'1a_notredame.jpg'
+        #path2 = r'1b_notredame.jpg'
+
+
+        #path1 = self.path1
+        #path2 = self.path2
+
+        # path1 = r'crop1.jpg'
+        # path2 = r'crop2.jpg'
+
+        self.img = cv2.imread(path1, 1)
+        self.img2 = cv2.imread(path2, 2)
+
+        # displaying the image
+        cv2.namedWindow("image 1", cv2.WINDOW_NORMAL)
+        cv2.imshow('image 1', self.img)
+        #cv2.namedWindow("image 2", cv2.WINDOW_NORMAL)
+        #cv2.imshow('image 2', self.img2)
+
+        # setting mouse hadler for the image
+        # and calling the click_event() function
+        cv2.setMouseCallback('image 1', self.click_event)
+        #cv2.setMouseCallback('image 2', self.click_event2)
+
+        # wait for a key to be pressed to exit
+
+        cv2.waitKey(0)
+        # close the window
+        cv2.destroyAllWindows()
+
+        #print('sx1 sy1', self.sx1, self.sy1)
+        #print('sx2 sy2', self.sx2, self.sy2)
+
+        points1, points2 = [], []
+        for x, y in zip(self.sx1, self.sy1):
+            points1.append((x, y))
+
+        points_1 = np.array(points1)
+
+        """for x, y in zip(self.sx2, self.sy2):
+            points2.append((x, y))
+
+        points_2 = np.array(points2)"""
+        print(points_1)
+        #np.save('p1.npy', points_1)
+        #np.save('p2.npy', points_2)
+
+    def driver2(self,img):
+        # reading the image
+        # path = r'D:\GaTech\TA - CV\ps05\ps05\ps5-1-b-1.png'
+        #path1 = r'1a_notredame.jpg'
+        #path2 = r'1b_notredame.jpg'
+
+
+        #path1 = self.path1
+        #path2 = self.path2
+
+        # path1 = r'crop1.jpg'
+        # path2 = r'crop2.jpg'
+
+        self.img = img
+
+
+        # displaying the image
+        cv2.namedWindow("image 1", cv2.WINDOW_NORMAL)
+        cv2.imshow('image 1', self.img)
+
+        # setting mouse hadler for the image
+        # and calling the click_event() function
+        cv2.setMouseCallback('image 1', self.click_event)
+        #cv2.setMouseCallback('image 2', self.click_event2)
+
+        # wait for a key to be pressed to exit
+
+        cv2.waitKey(0)
+        # close the window
+        cv2.destroyAllWindows()
+
+        #print('sx1 sy1', self.sx1, self.sy1)
+        #print('sx2 sy2', self.sx2, self.sy2)
+
+        points1 = []
+        for x, y in zip(self.sx1, self.sy1):
+            points1.append((x, y))
+
+        points_1 = np.array(points1)
+
+        """for x, y in zip(self.sx2, self.sy2):
+            points2.append((x, y))
+
+        points_2 = np.array(points2)"""
+        #print(points_1)
+
+        
+
+
+        #np.save('p1.npy', points_1)
+        #np.save('p2.npy', points_2)
+
+
+
+
+test_image = cv2.imread("231_tt1028532.jpg")
+#cv2.imshow("idiot stupid mf", test_image)
+#cv2.waitKey(0)
+
+
+Mouse_Click_Correspondence().driver2(test_image)
+
+
+#for i in sorted(os.listdir('.')):
+
+    
+"""
+        if i[-3:] == 'jpg':
         if int(i.split('_')[0]) < 126:
             continue
 
         image = cv2.imread(i)
+        #img = cv2.imread(path1, 1)
+
         print(i)
         cv2.imshow(i, image)
         cv2.waitKey(-1)
+"""
